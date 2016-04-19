e60f62429a169be62241cc3482eae5e5b5b10d1e
Grayson112233 <graysonpike@Graysons-MBP.attlocal.net>
2016-04-18 22:08:36 -0500

    Finished basic voting system

--- test_website/polls/urls.py
+++ test_website/polls/urls.py
@@ -3,12 +3,11 @@ from django.conf.urls import url
 # Import views form the poll app (this directory)
 from . import views
 
-urlpatterns = [
-	# This object constuctor is taking a regex, the index
-	# view function, and the name 'index' 
-	url(r'^$', views.index, name="index"),
-        url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
-        url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
-        url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
-]
+app_name = 'polls'
 
+urlpatterns = [
+	url(r'^$', views.index, name="index"),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
+]
--- test_website/polls/views.py
+++ test_website/polls/views.py
@@ -1,24 +1,35 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Question 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from .models import Question, Choice
 
 # This method will handle responses to the index page
 def index(request):
-    # List 5 most recent posts delineated by a comma
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    context = { 'latest_questions': latest_questions }
-    return render(request, 'polls/index.html', context)
+	# List 5 most recent posts delineated by a comma
+	latest_questions = Question.objects.order_by('-pub_date')[:5]
+	context = { 'latest_questions': latest_questions }
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
-    context = { 'question': question }
-    return render(request, "polls/detail.html", context)
+	question = get_object_or_404(Question, pk=question_id)
+	context = { 'question': question }
+	return render(request, "polls/detail.html", context)
 
 def results(request, question_id):
-    return HttpResponse("This is the results of question " + str(question_id))
+	question = get_object_or_404(Question, pk=question_id)
+	context = { 'question': question }
+	return render(request, "polls/results.html", context)
 
 def vote(request, question_id):
-    return HttpResponse("This is the voting page of question " + str(question_id))
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "No choice was selected"
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
