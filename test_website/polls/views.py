e142d23e81632aaf5e2007d8f87c4e2113aa2ae7
Grayson112233 <graysonpike@Graysons-MBP.attlocal.net>
2016-04-19 19:01:55 -0500

    Using generic views

--- test_website/polls/views.py
+++ test_website/polls/views.py
@@ -1,24 +1,24 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views import generic
 from .models import Question, Choice
 
 # This method will handle responses to the index page
+
 def index(request):
 	# List 5 most recent posts delineated by a comma
 	latest_questions = Question.objects.order_by('-pub_date')[:5]
 	context = { 'latest_questions': latest_questions }
 	return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	context = { 'question': question }
-	return render(request, "polls/detail.html", context)
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	context = { 'question': question }
-	return render(request, "polls/results.html", context)
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
